241ac7362166d1c6976d37bb420298f5f76dd27f
Bukovski <kmanankov@bk.ru>
2018-08-16 14:39:18 +0300

    update index

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const db = require('./lib/db');
 const server = require('./lib/server');
 
-
-/*(async () => {
-  const objConnection = await db.connection()
-  try {
-    const result = await db.query(objConnection, `SELECT * FROM workers`);
-    // console.log(JSON.stringify(result));
-    console.log(result);
-  } finally {                                          //очистка, закрываем соединение с БД!
-    objConnection.end();
-  }
-})().catch(e => console.error(e));*/
-
 const app = {};
 
 app.init = () => {
-  
-  // Start the server
-  server.init();
-  
-  
+  server.init(); // Start the server
 };
 
 if (require.main === module) { //run file
